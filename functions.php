748fc746fd841b0026d3ce73f6ebbdc0b8df77ce
Amr Aly <amr.aly57@yahoo.com>
2018-09-30 16:32:53 +0200

    Added WP bootstrap navwalker

--- functions.php
+++ functions.php
@@ -1,8 +1,15 @@
 <?php
 
+require_once('class-wp-bootstrap-navwalker.php');
+
 function theme_setup() {
     // add thumbnail support
     add_theme_support('post-thumbnails');
+
+    // Register menus
+    register_nav_menus( array(
+        'primary' => __('Primary Menu'),
+    ) );
 }
 
 add_action('after_setup_theme', 'theme_setup');
--- header.php
+++ header.php
@@ -21,10 +21,18 @@
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-               
-                <form class="form-inline my-2 my-lg-0">
-              
-                </form>
+  <?php             
+        wp_nav_menu( array(
+            'theme_location'  => 'primary',
+            'depth'	          => 2, // 1 = no dropdowns, 2 = with dropdowns.
+            'container'       => false,
+            // 'container_class' => 'collapse navbar-collapse',
+            'container_id'    => 'bs-example-navbar-collapse-1',
+            'menu_class'      => 'navbar-nav mr-auto',
+            'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+            'walker'          => new WP_Bootstrap_Navwalker(),
+        ) );
+    ?>
             </div>
         </div>
     </nav>
