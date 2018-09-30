e85e573caece75c3d6a6079d806b1bf9d46cef2b
Amr Aly <amr.aly57@yahoo.com>
2018-09-30 03:39:18 +0200

    initail commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bootstrap WP Theme</title>
+
+    <link rel="stylesheet" href="<?php echo  get_stylesheet_directory_uri() . '/css/bootstrap.min.css' ?>">
+
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>">
+    
+</head>
+<body>
+
+    <h1>Hello World</h1>
+
+</body>
+</html>
